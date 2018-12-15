159ef1ea50f84c6302e050e45c5d85d290a1aba3
rijkvanzanten <rijkvanzanten@me.com>
2018-12-15 14:49:32 -0500

    Setup server boilerplate and add content

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+module.exports = express()
+  .set("view engine", "ejs")
+  .set("views", "./views")
+  .use(express.static("./public"))
+  .get("/", renderHome)
+  .get("/:slug", renderSingle)
+  .all("*", notFound);
+
+function renderHome(req, res) {
+  res.render("index");
+}
+
+function renderSingle(req, res) {
+  res.render("single");
+}
+
+function notFound(req, res) {
+  res.render("not-found");
+}
