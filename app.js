3525fe1d4f5a00428b562d209d8ee1f93890795f
rijkvanzanten <rijkvanzanten@me.com>
2018-12-16 11:54:19 -0500

    Add homepage markup

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const AWS = require("aws-sdk");
 
 require("dotenv").config();
+
+AWS.config = new AWS.Config();
+AWS.config.accessKeyId = process.env.AWS_ID;
+AWS.config.secretAccessKey = process.env.AWS_KEY;
+AWS.config.region = process.env.AWS_REGION;
+
+const db = new AWS.DynamoDB.DocumentClient();
 
 module.exports = express()
   .set("view engine", "ejs")
@@ -11,7 +19,27 @@ module.exports = express()
   .all("*", notFound);
 
 function renderHome(req, res) {
-  res.render("index");
+  // DynamoDB doesn't support a way to "query" data by a specific date range
+  // Therefore, the next best thing we can do is retrieve all the items and display
+  // those. 
+  // I was hoping to fetch just the last 7 items sorted by date, but alas.
+  const params = {
+    TableName: "deardiary",
+    ProjectionExpression: "slug, author, image, title"
+  };
+
+  const data = db.scan(params, function(error, data) {
+    if (error) {
+      console.log(error);
+      res.status(500).end();
+    }
+
+    const items = data.Items.sort((a, b) => {
+      return new Date(a.datetime) < new Date(b.datetime) ? 1 : -1;
+    });
+
+    res.render("index", { items });
+  });
 }
 
 function renderSingle(req, res) {
